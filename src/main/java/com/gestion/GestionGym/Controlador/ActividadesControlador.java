252f13e7922dfd9f9781c63bf377336e7f834298
Luisbautista01 <bautistaluisantonio24@gmail.com>
2024-07-29 14:09:27 -0500

    [feature][task 12][12]

--- src/main/java/com/gestion/GestionGym/Controlador/ActividadesControlador.java
+++ src/main/java/com/gestion/GestionGym/Controlador/ActividadesControlador.java
@@ -39,7 +39,7 @@ public class ActividadesControlador {
                     fechaEntrenamiento, tipoEntrenamiento, duracionEntrenamiento);
             return ResponseEntity.status(HttpStatus.CREATED).body("Actividad enviada correctamente.");
         } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 
